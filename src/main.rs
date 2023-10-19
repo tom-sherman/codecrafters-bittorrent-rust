cc95be5b65214d0ca32afd118dba09e193d798af
Tom Sherman <the.tomsherman@gmail.com>
2023-10-19 16:25:38 +0100

    Parsing is boring, use serde_bencode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
 use serde_json;
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if let Some((length, str)) = encoded_value.split_once(":") {
-        if let Ok(length) = length.parse::<usize>() {
-            return serde_json::Value::String(str[..length].to_string());
-        }
-    } else if encoded_value.starts_with("i") && encoded_value.ends_with("e") {
-        if let Ok(number) = encoded_value[1..encoded_value.len() - 1].parse::<i64>() {
-            return serde_json::Value::Number(serde_json::Number::from(number));
-        }
-    }
-    panic!("Unhandled encoded value: {}", encoded_value)
-}
-
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,7 +8,7 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = serde_bencode::from_str::<serde_json::Value>(&encoded_value).unwrap();
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
