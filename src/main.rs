0d6ead54c567f36527e069e2333e39dbbd6e7de9
Tom Sherman <the.tomsherman@gmail.com>
2023-10-19 15:06:33 +0100

    Ints

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::env;
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     if let Some((length, str)) = encoded_value.split_once(":") {
         if let Ok(length) = length.parse::<usize>() {
             return serde_json::Value::String(str[..length].to_string());
+        }
+    } else if encoded_value.starts_with("i") && encoded_value.ends_with("e") {
+        if let Ok(number) = encoded_value[1..encoded_value.len() - 1].parse::<i64>() {
+            return serde_json::Value::Number(serde_json::Number::from(number));
         }
     }
     panic!("Unhandled encoded value: {}", encoded_value)
@@ -20,7 +23,6 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // Uncomment this block to pass the first sta
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
